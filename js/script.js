4bb9d57a576d276a80ea63623707ee9464c47457
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2024-01-04 10:21:57 +0700

    Add Button Cari in Form search

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 const navbarList = document.querySelector(".navbar-list");
 
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarList.classList.toggle("active")
-}
+  navbarList.classList.toggle("active");
+};
 
 const hamburger = document.querySelector("#hamburger-menu");
-document.addEventListener("click", function(e){
-    if(!hamburger.contains(e.target) && !navbarList.contains(e.target)){
-        navbarList.classList.remove("active")
-    }
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarList.contains(e.target)) {
+    navbarList.classList.remove("active");
+  }
 });
 
-const inputForm = document.querySelector(".input-form");
+const form = document.querySelector(".form");
+const btnsearch = document.querySelector(".btnsearch");
+const icon = document.getElementById("iconsearch");
 
 document.querySelector("#search").onclick = () => {
-    inputForm.classList.toggle("active-form")
-}
+  form.classList.toggle("active-form");
+  if (icon.classList.contains("fa-magnifying-glass")) {
+    icon.classList.remove("fa-magnifying-glass");
+    icon.classList.add("fa-xmark");
+  } else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-magnifying-glass");
+  }
+};
